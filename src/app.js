86b5271409ea733192592a4f998d4b4b958fea6d
pedrocerto <pedro.certo@aluno.ufabc.edu.br>
2022-07-15 22:17:56 -0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function fazerReserva(){
+    var data = document.querySelector("#data").value;
+    var horas = document.querySelector("#horas").value;
+    var ticket = document.getElementById('ticket');
+
+    var valor = horas * 300.0
+
+    if(confirm(`Reserva de ${horas}h para ${data} sai por R$${valor},00. Deseja confirmar a reserva?`)){
+        ticket.value = "Reserva confirmada!"
+    }
+}
